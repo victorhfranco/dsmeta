30f131b3a1797ed98c62abd62d288167f9093168
Victor Henrique Franco <victor.hfranco@yahoo.com>
2022-07-13 12:33:32 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+	//Salvar e deletar vendas do banco de dados em JPA
+}
